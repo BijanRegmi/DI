3d6c023ca1fd693a4c64d2e3e621922cebd572db
(void *)0x0 <55084653+BijanRegmi@users.noreply.github.com>
2023-11-19 01:06:36 +0545

    Auto resolution

--- index.ts
+++ index.ts
@@ -1,68 +1,76 @@
-type ClassType = 'transient' | 'singleton';
-type ObjectWithConstructor = { constructor: Function };
-type InjectableOptions = { type: ClassType };
+import { v4 as uuid } from 'uuid';
+import 'reflect-metadata';
 
-type ContainerRecord<T extends ObjectWithConstructor> = {
-    instance: T;
-    type: ClassType;
-    constructorFn: Function;
-};
+type Constructor = new (...args: any[]) => any;
+type LifeSpan = 'transient' | 'singleton';
+type InjectableOptions = { type: LifeSpan };
+type InstanceType<T extends Constructor> = T extends new (...args: any[]) => infer R ? R : never;
 
 class DependencyService {
     private container: {
-        [name: string]: ContainerRecord<any>;
+        [name: string]: any;
     } = {};
 
-    registerService<T extends ObjectWithConstructor>(instance: T): ContainerRecord<T> {
-        const record: ContainerRecord<T> = {
-            instance,
-            type: 'singleton',
-            constructorFn: instance.constructor,
-        };
-        this.container[instance.constructor.name] = record;
-        return record;
+    get<T extends Constructor>(target: T): InstanceType<T> {
+        const injectable = Reflect.getMetadata('__injectable', target);
+        if (!injectable) {
+            throw new Error(
+                `${target.name} is not injectable. Add @Injectable() decorator to make the class injectable.`,
+            );
+        }
+
+        const lifespan: LifeSpan = Reflect.getMetadata('__lifespan', target);
+        const params = Reflect.getMetadata('design:paramtypes', target);
+
+        if (lifespan == 'transient') {
+            const args = params.map((param: any) => this.get(param));
+            return new target(...args) as InstanceType<T>;
+        }
+
+        const record = this.container[target.name];
+        if (record) {
+            return record;
+        }
+
+        const args = params.map((param: any) => this.get(param));
+        const instance: InstanceType<T> = new target(...args);
+        this.container[target.name] = instance;
+
+        return instance;
     }
 
-    getService<T extends new (...args: any[]) => any>(target: T) {
-        const record = this.container[target.name];
-        return record ? (record.instance as T) : null;
+    all() {
+        return this.container;
     }
 }
 
 const dService = new DependencyService();
 
 const Injectable = (options: InjectableOptions = { type: 'singleton' }) => {
-    return <T extends new (...args: any[]) => any>(target: T) => {
-        return class {
-            constructor(...args: any[]) {
-                if (options.type == 'transient') return new target(...args);
-
-                const existingInstance = dService.getService(target);
-                if (existingInstance) {
-                    return existingInstance;
-                }
-
-                const newInstance = new target(...args);
-                dService.registerService(newInstance);
-                return newInstance;
-            }
-        } as T;
+    return <T extends Constructor>(target: T) => {
+        Object.defineProperty(target, 'name', { value: uuid() });
+        Reflect.defineMetadata('__injectable', true, target);
+        Reflect.defineMetadata('__lifespan', options.type, target);
     };
 };
 
 @Injectable()
 class ServiceOne {
-    private myNum: number;
-
-    constructor(num: number) {
-        this.myNum = num;
-    }
+    constructor() { }
 
     getNumber() {
-        return this.myNum;
+        return 'Value from Service One';
     }
 }
 
-const x = new ServiceOne(2);
-const y = new ServiceOne(3);
-console.log(x.getNumber(), y.getNumber());
+@Injectable()
+class ServiceTwo {
+    constructor(private serviceOne: ServiceOne) { }
+
+    getNumber() {
+        return 'Value from Service Two' + '...' + this.serviceOne.getNumber();
+    }
+}
+
+const servTwo = dService.get(ServiceTwo);
+console.log(servTwo.getNumber());
